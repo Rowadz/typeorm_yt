c34ad1798baa060feae3176084a91489e2c657bd
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-04 01:44:15 +0300

    all the entites

--- app.ts
+++ app.ts
@@ -1,13 +1,23 @@
 import 'reflect-metadata';
 import { createConnection, Connection } from 'typeorm';
-import { UserEntity, PostsEntity, CategoriesEntity } from './entities';
+import {
+  UserEntity,
+  PostsEntity,
+  CategoriesEntity,
+  CategoriesPostsEntity,
+} from './entities';
 import { createUsers } from './crud/';
 
 const app = async () => {
   const connection: Connection = await createConnection({
     type: 'sqlite',
     database: './db/testing_typeorm.db',
-    entities: [UserEntity, PostsEntity, CategoriesEntity],
+    entities: [
+      UserEntity,
+      PostsEntity,
+      CategoriesEntity,
+      CategoriesPostsEntity,
+    ],
     // host: 'localhost',
     // port: 3306,
     // username: 'test',
@@ -17,7 +27,7 @@ const app = async () => {
   // Creates database schema for all entities registered in this connection.
   // Can be used only after connection to the database is established.
   // pass true to drop everything b4 creating anything
-  await connection.synchronize(true);
+  await connection.synchronize(true).catch(console.error);
   await createUsers(connection);
 };
 
