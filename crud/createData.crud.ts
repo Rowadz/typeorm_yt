77b33676b3d14b2df4c59f24fa1d687ccea58878
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-03 06:06:08 +0300

    examples on find

--- crud/createData.crud.ts
+++ crud/createData.crud.ts
@@ -1,6 +1,14 @@
-import { Connection } from 'typeorm';
+import {
+  Connection,
+  Repository,
+  LessThanOrEqual,
+  LessThan,
+  Raw,
+} from 'typeorm';
 import { name, internet, random, date, lorem } from 'faker';
 import { UserEntity, PostsEntity } from '../entities';
+import { writeFileSync } from 'fs';
+import { userInfo } from 'os';
 
 const createUsers = async (con: Connection) => {
   const users: Array<UserEntity> = [];
@@ -34,6 +42,20 @@ const createPosts = async (con: Connection, users: Array<UserEntity>) => {
     await con.manager.save(post1);
     await con.manager.save(post2);
   }
+  await readUsers(con);
+};
+
+const readUsers = async (con: Connection) => {
+  const userRepository: Repository<UserEntity> = con.getRepository(UserEntity);
+  const data = await userRepository.find();
+  //   const data = await userRepository.find({ order: { birthDate: 'ASC' } });
+  //   const data = await userRepository.find({ take: 1, skip: 6 });
+  //   const data = await userRepository.findOne(8);
+  //   const data = await userRepository.find({ relations: ['posts'] });
+  //   const data = await userRepository.find({
+  //     where: { currentDate: Raw((alias) => `${alias} < NOW()`) },
+  //   });
+  writeFileSync('data.json', JSON.stringify(data, null, 2));
 };
 
 export { createUsers };
