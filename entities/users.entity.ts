d701cf75e048b97efcec954facba38ebe09133a9
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-03 03:24:50 +0300

    users: one-to-many

--- entities/users.entity.ts
+++ entities/users.entity.ts
@@ -1,10 +1,4 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  ManyToOne,
-  JoinColumn,
-} from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { SharedProp } from './sharedProp.helper';
 import { PostsEntity } from './posts.entity';
 
@@ -31,7 +25,7 @@ export class UserEntity extends SharedProp {
   @Column({ nullable: false })
   password: string;
 
-  @ManyToOne(() => PostsEntity, (post: PostsEntity) => post.user, {
+  @OneToMany(() => PostsEntity, (post: PostsEntity) => post.user, {
     onUpdate: 'CASCADE',
     onDelete: 'CASCADE',
   })
