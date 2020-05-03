2b3d1f00cfa055c334426881d005ba7596cefe75
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-04 01:35:39 +0300

    m:m

--- entities/posts.entity.ts
+++ entities/posts.entity.ts
@@ -4,9 +4,11 @@ import {
   Column,
   ManyToOne,
   JoinColumn,
+  OneToMany,
 } from 'typeorm';
 import { SharedProp } from './sharedProp.helper';
 import { UserEntity } from './users.entity';
+import { CategoriesPostsEntity } from './categoriesPosts.entity';
 
 @Entity({ name: 'posts' })
 export class PostsEntity extends SharedProp {
@@ -26,4 +28,10 @@ export class PostsEntity extends SharedProp {
   // or custom referenced column.
   @JoinColumn({ name: 'user_id' })
   user: UserEntity;
+
+  @OneToMany(
+    () => CategoriesPostsEntity,
+    (categoriesPosts: CategoriesPostsEntity) => categoriesPosts.post
+  )
+  categoriesPosts: Array<CategoriesPostsEntity>;
 }
