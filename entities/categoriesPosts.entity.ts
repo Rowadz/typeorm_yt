f35cd028f1994ce22e5c9b8badbcad8b0bd9bc15
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-04 02:45:05 +0300

    constructor

--- entities/categoriesPosts.entity.ts
+++ entities/categoriesPosts.entity.ts
@@ -11,6 +11,16 @@ import { CategoriesEntity } from './categories.entity';
 
 @Entity({ name: 'categories_posts' })
 export class CategoriesPostsEntity extends SharedProp {
+  constructor(
+    someColumn: string,
+    post: PostsEntity,
+    category: CategoriesEntity
+  ) {
+    super();
+    this.someColumn = someColumn;
+    this.post = post;
+    this.category = category;
+  }
   @PrimaryGeneratedColumn()
   id: number;
 
