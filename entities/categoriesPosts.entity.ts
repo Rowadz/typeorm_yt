c71cf385d92d535782d8d34397af9d354bf31975
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-04 01:44:29 +0300

    joinCol

--- entities/categoriesPosts.entity.ts
+++ entities/categoriesPosts.entity.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
 import { SharedProp } from './sharedProp.helper';
 import { PostsEntity } from './posts.entity';
 import { CategoriesEntity } from './categories.entity';
@@ -12,11 +18,13 @@ export class CategoriesPostsEntity extends SharedProp {
   someColumn: string;
 
   @ManyToOne(() => PostsEntity, (post: PostsEntity) => post.categoriesPosts)
+  @JoinColumn({ name: 'post_id' })
   post: PostsEntity;
 
   @ManyToOne(
     () => CategoriesEntity,
     (category: CategoriesEntity) => category.categoriesPosts
   )
+  @JoinColumn({ name: 'category_id' })
   category: CategoriesEntity;
 }
