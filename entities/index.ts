0c467226ef51534425ab8d9366b5fb0fcdd0635e
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-04 01:44:42 +0300

    export cate-posts

--- entities/index.ts
+++ entities/index.ts
@@ -1,3 +1,4 @@
 export { UserEntity } from './users.entity';
 export { PostsEntity } from './posts.entity';
 export { CategoriesEntity } from './categories.entity';
+export { CategoriesPostsEntity } from './categoriesPosts.entity';
