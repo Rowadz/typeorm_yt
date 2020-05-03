6013416ac96e7ec5ec7552ba2c1f41201fb9e5a0
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-04 01:30:10 +0300

    for sqllite

--- entities/categories.entity.ts
+++ entities/categories.entity.ts
@@ -1,22 +1,27 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { SharedProp } from './sharedProp.helper';
 
-enum CategoriesLabels {
-  coffee = 'coffee',
-  snacks = 'snacks',
-  time = 'time',
-  programming = 'programming',
-}
+// postgresql example enum
+// enum CategoriesLabels {
+//   coffee = 'coffee',
+//   snacks = 'snacks',
+//   time = 'time',
+//   programming = 'programming',
+// }
 
 @Entity({ name: 'categories' })
 export class CategoriesEntity extends SharedProp {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({
-    type: 'enum',
-    enum: CategoriesLabels,
-    default: CategoriesLabels.programming,
-  })
+  @Column()
   label: string;
+
+  //   postgresql example enum
+  //   @Column({
+  //     type: 'enum',
+  //     enum: CategoriesLabels,
+  //     default: CategoriesLabels.programming,
+  //   })
+  //   label: string;
 }
